93e779dabf16d3f2636534aad8771419d19033ab
Giuseppe Maxia <g.maxia@gmail.com>
2017-05-06 15:40:02 +0200

    Revert "Added colume removal to cleanup_cluster.sh"
    This reverts commit baaafed0778e9119278b1ab1340bf369ff96469f.

--- cleanup_cluster.sh
+++ cleanup_cluster.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Stopping and removing the mysqlgr1 container..."
-docker stop mysqlgr1 >/dev/null 2>&1 && docker rm -v mysqlgr1 >/dev/null 2>&1
+docker stop mysqlgr1 >/dev/null 2>&1 && docker rm mysqlgr1 >/dev/null 2>&1
 
 echo "Stopping and removing the mysqlgr2 container..."
-docker stop mysqlgr2 >/dev/null 2>&1 && docker rm -v mysqlgr2 >/dev/null 2>&1
+docker stop mysqlgr2 >/dev/null 2>&1 && docker rm mysqlgr2 >/dev/null 2>&1
 
 echo "Stopping and removing the mysqlgr3 container..."
-docker stop mysqlgr3 >/dev/null 2>&1 && docker rm -v mysqlgr3 >/dev/null 2>&1
+docker stop mysqlgr3 >/dev/null 2>&1 && docker rm mysqlgr3 >/dev/null 2>&1
 
 echo "Stopping and removing the mysqlrouter1 container..."
-docker stop mysqlrouter1 >/dev/null 2>&1 && docker rm -v mysqlrouter1 >/dev/null 2>&1
+docker stop mysqlrouter1 >/dev/null 2>&1 && docker rm mysqlrouter1 >/dev/null 2>&1
 
 echo "Removing the grnet network..."
 docker network rm grnet >/dev/null 2>&1
