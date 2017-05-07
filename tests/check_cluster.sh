7eb308d97482ae5adc4b8a58ad7956864f0bb45c
Giuseppe Maxia <g.maxia@gmail.com>
2017-05-07 22:09:15 +0200

    Added command to test the cluster status

---
+++ tests/check_cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+node=$1
+[ -z "$node" ] && node=1
+
+PWD_FILE=secretpassword.txt
+if [ ! -f $PWD_FILE ] ; then
+    # if the script is executed in the sub folder, the password file is one level up
+    PWD_FILE=../$PWD_FILE
+fi
+# if you want to view the command that's being executed, uncomment the set -x line
+# set -x 
+docker exec -it mysqlgr$node mysqlsh \
+    --uri=root:$(cat $PWD_FILE)@mysqlgr$node:3306 \
+    -i -e 'dba.getCluster().status()'
+
