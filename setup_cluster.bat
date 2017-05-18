70edbea8324d2c01217433196e000d813969b745
Matt Lord <matt.lord@oracle.com>
2017-05-18 11:10:36 -0400

    Example batch script for Windows

---
+++ setup_cluster.bat
@@ -0,0 +1,25 @@
+title 3 Node Group Replication Cluster Setup
+
+echo Creating dedicated network: grnet ...
+docker network create --driver bridge grnet
+
+echo Bootstrapping the cluster ...
+docker run --rm --name=mysqlgr1 --hostname=mysqlgr1 --network=grnet -p 3391:3391 -e MYSQL_ROOT_PASSWORD=root -e BOOTSTRAP=1 -e GROUP_NAME="92bb4382-3bd1-11e7-a919-92ebcb67fe33" -itd mattalord/innodb-cluster
+timeout 3 /NOBREAK
+
+echo Adding second node to the cluster ...
+docker run --rm --name=mysqlgr2 --hostname=mysqlgr2 --network=grnet -p 3392:3392 -e MYSQL_ROOT_PASSWORD=root -e GROUP_NAME="92bb4382-3bd1-11e7-a919-92ebcb67fe33" -e GROUP_SEEDS="mysqlgr1:6606" -itd mattalord/innodb-cluster
+timeout 3 /NOBREAK
+
+echo Adding a third node to the cluster ...
+docker run --rm --name=mysqlgr3 --hostname=mysqlgr3 --network=grnet -p 3393:3393 -e MYSQL_ROOT_PASSWORD=root -e GROUP_NAME="92bb4382-3bd1-11e7-a919-92ebcb67fe33" -e GROUP_SEEDS="mysqlgr1:6606" -itd mattalord/innodb-cluster
+timeout 3 /NOBREAK
+
+echo Giving the cluster time to finish initializing and syncing up...
+; the setup is *really* slow on Windows (at least for me)
+timeout 30 /NOBREAK
+
+echo Testing the cluster status ...
+docker exec -it mysqlgr1 mysql -u root -proot -e "select * from performance_schema.replication_group_members"
+
+echo Done!
